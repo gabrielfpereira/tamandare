5f90bd8eb17a28e1513e4ac680f385f0e0ccd96f
gabrielfpereira <gabrielf.pereira@outlook.com>
2023-08-05 10:46:06 -0300

    pint

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    if(app()->isLocal()){
+    if(app()->isLocal()) {
         auth()->loginUsingId(1);
     }
+
     return redirect()->route('dashboard');
 });
 
