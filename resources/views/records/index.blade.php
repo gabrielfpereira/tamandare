0a49f8b86ef2435be9051742653b461ccec5d0d0
gabrielfpereira <gabrielf.pereira@outlook.com>
2023-09-10 18:45:46 -0300

    pesquisa responsiva

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -10,16 +10,16 @@
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
-                    <div class="flex items-center justify-between mb-6">
-                        <div class="w-2/3 mb-6">
+                    <div class="items-center justify-between mb-6 sm:flex">
+                        <div class="mb-6 sm:w-2/3">
                             <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pesquisar</label>
-                            <form class="flex items-center mr-3" action="{{ route('records.index') }}" method="GET">
+                            <form class="items-center mr-3 sm:flex" action="{{ route('records.index') }}" method="GET">
                                 @csrf
                                 @method('GET')
                                 <input name="search" placeholder="Digite o nome do aluno" type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <input name="search_class" placeholder="Digite a turma do aluno" type="text" id="base-input" class="ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <input name="search_date" placeholder="pesquise por uma data" type="date" id="base-input" class="ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <button class="ml-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Pesquisar</button>
+                                <input name="search_class" placeholder="Digite a turma do aluno" type="text" id="base-input" class="sm:ml-2 mt-2 sm:mt-0 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <input name="search_date" placeholder="pesquise por uma data" type="date" id="base-input" class="sm:ml-2 mt-2 sm:mt-0 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <button class="sm:ml-3 mt-2 sm:mt-0 w-full sm:w-24 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Pesquisar</button>
                             </form>
                         </div>
 
